4aaf66fc513c4f2a7c6394e9d2196db92b902c65
aboanasanam42-cpu <2.93862144e+08+aboanasanam42-cpu@users.noreply.github.com>
2026-07-29 06:13:11 +0000

    Bypass strict signing to generate APK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,7 +33,7 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = false
-            signingConfig = signingConfigs.getByName("release")
+            // signingConfig = signingConfigs.getByName("release") // Disable strict signing temporarily
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
